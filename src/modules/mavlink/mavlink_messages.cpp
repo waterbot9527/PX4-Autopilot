2f89fc7315367790b1ab209944f26e41f55b10e3
ShiTouchaozai <1820076782@qq.com>
2025-09-22 20:54:47 +0800

    recover file

--- src/modules/mavlink/mavlink_messages.cpp
+++ src/modules/mavlink/mavlink_messages.cpp
@@ -157,7 +157,6 @@
 # include "streams/UAVIONIX_ADSB_OUT_DYNAMIC.hpp"
 #endif // !CONSTRAINED_FLASH
 
-#include "streams/WBOT_CONTROL_MOTO.hpp"
 
 // ensure PX4 rotation enum and MAV_SENSOR_ROTATION align
 static_assert(MAV_SENSOR_ROTATION_NONE == static_cast<MAV_SENSOR_ORIENTATION>(ROTATION_NONE),
--- src/modules/mavlink/streams/WBOT_CONTROL_MOTO.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef WBOT_CONTROL_HPP
-#define WBOT_CONTROL_HPP
-
-
-#include <uORB/topics/wbot_ctrl_led.h>
-#include <uORB/topics/wbot_ctrl_moto.h>
-
-#endif
-
